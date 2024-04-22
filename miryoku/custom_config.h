f8bf832c2a386a5b8d57d3d4713b6a50a195ad96
Laurent <seres@seres.eu.org>
2024-04-22 12:48:45 +0200

    now maybe?

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -11,6 +11,24 @@
       &kp COMMA, &kp DOT, U_NP, U_NP, &kp ESC, &kp SPACE, &kp TAB, &kp RET,    \
       &kp BSPC, &kp DEL, U_NP, U_NP
 
+#define MIRYOKU_ALTERNATIVES_NAV                                               \
+  U_BOOT, &u_to_U_TAP, &u_to_U_EXTRA, &u_to_U_BASE, &u_to_U_GAME, U_RDO,       \
+      U_PST, U_CPY, U_CUT, U_UND, &kp LGUI, &kp LALT, &kp LCTRL, &kp LSHFT,    \
+      U_NA, &u_caps_word, &kp LEFT, &kp DOWN, &kp UP, &kp RIGHT, U_NA,         \
+      &kp RALT, &u_to_U_NUM, &u_to_U_NAV, U_NA, &kp INS, &kp HOME, &kp PG_DN,  \
+      &kp PG_UP, &kp END, U_NP, U_NP, U_NA, U_NA, U_NA, &kp RET, &kp BSPC,     \
+      &kp DEL, U_NP, U_NP
+
+#define XXX &none
+
+#define MIRYOKU_LAYOUTMAPPING_CORNE(                                           \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K10, K11, K12, K13, K14, \
+    K15, K16, K17, K18, K19, K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, \
+    N30, N31, K32, K33, K34, K35, K36, K37, N38, N39)                          \
+  XXX K00 K01 K02 K03 K04 K05 K06 K07 K08 K09 XXX XXX K10 K11 K12 K13 K14 K15  \
+      K16 K17 K18 K19 XXX XXX K20 K21 K22 K23 K24 K25 K26 K27 K28 K29 XXX K32  \
+          K33 K34 K35 K36 K37
+
 #define MIRYOKU_LAYER_LIST                                                     \
   MIRYOKU_X(BASE, "Base")                                                      \
   MIRYOKU_X(EXTRA, "Extra")                                                    \
@@ -24,6 +42,8 @@
   MIRYOKU_X(FUN, "Fun")                                                        \
   MIRYOKU_X(GAME, "Game")
 
+#define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
+
 #define U_BASE 0
 #define U_EXTRA 1
 #define U_TAP 2
@@ -35,5 +55,3 @@
 #define U_SYM 8
 #define U_FUN 9
 #define U_GAME 10
-
-#define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
