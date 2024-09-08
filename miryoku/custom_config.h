945d7232927e2701387daad2e9b72dcff6820cf9
Laurent <seres@seres.eu.org>
2024-09-09 01:25:14 +0200

    added gw2 layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,24 +6,38 @@
 #define XXX &none
 
 #define MIRYOKU_LAYER_GAME                                                     \
-  &kp LCTRL, &kp Q, &kp W, &kp E, &kp R, &kp T, &kp Y, &kp U, &kp I, &kp O, \
-    &kp LSHFT, &kp A, &kp S, &kp D, &kp F, &kp G, &kp H, &kp J, &kp K, &kp L,    \
-    &kp LALT, &kp Z, &kp X, &kp C, &kp V, &kp B, &kp N, &kp M, &kp COMMA, &u_to_U_BASE,    \
-    U_NP, U_NP, &kp ESC, &kp SPACE, &kp TAB, &kp RET, U_LT(U_NUM, BSPC), &kp DEL,     \
-    U_NP, U_NP
+  &kp LCTRL, &kp Q, &kp W, &kp E, &kp R, &kp T, &kp Y, &kp U, &kp I, &kp O,    \
+      &kp LSHFT, &kp A, &kp S, &kp D, &kp F, &kp G, &kp H, &kp J, &kp K,       \
+      &kp L, &kp LALT, &kp Z, &kp X, &kp C, &kp V, &kp B, &kp N, &kp M,        \
+      &kp COMMA, &u_to_U_BASE, U_NP, U_NP, &kp ESC, &kp SPACE, &kp TAB,        \
+      &kp RET, U_LT(U_NUM, BSPC), &kp DEL, U_NP, U_NP
 
-#define MIRYOKU_LAYER_POE                                                     \
-  &kp Q, &kp W, &kp E, &kp R, &kp T, &kp Y, &kp U, &kp I, &kp O, &kp P, \
-    &kp A, &kp S, &kp D, &kp F, &kp G, &kp H, &kp J, &kp K, &kp L, &kp SQT,    \
-    &kp Z, &kp X, &kp C, &kp V, &kp B, &kp N, &kp M, &kp COMMA, &kp DOT, &u_to_U_BASE,    \
-    U_NP, U_NP, U_MT(LCTRL, ESC), U_MT(LALT, SPACE), U_MT(LSHIFT, TAB), &kp RET, U_LT(U_NUM, BSPC), &kp DEL,     \
-    U_NP, U_NP
+#define MIRYOKU_LAYER_LOL                                                      \
+  &kp Q, &kp W, &kp E, &kp R, &kp T, &kp Y, &kp U, &kp I, &kp O, &kp P, &kp A, \
+      &kp S, &kp D, &kp F, &kp G, &kp H, &kp J, &kp K, &kp L, &kp SQT, &kp Z,  \
+      &kp X, &kp C, &kp V, &kp B, &kp N, &kp M, &kp COMMA, &kp DOT,            \
+      &u_to_U_BASE, U_NP, U_NP, U_MT(LCTRL, ESC), U_MT(LALT, SPACE),           \
+      U_MT(LSHIFT, TAB), &kp RET, U_LT(U_NUM, BSPC), &kp DEL, U_NP, U_NP
+
+#define MIRYOKU_LAYER_GW2                                                      \
+      &kp Q, &kp E, &kp W, &kp R, &kp T,           &kp Y, &kp U, &kp I, &kp O, &kp P,    \
+      &kp G, &kp A, &kp S, &kp D, &kp F,           &kp H, &kp J, &kp K,&kp L, &kp SQT, \
+      &kp Z, &kp X, &kp C, &kp V, &kp B,           &kp N, &kp M, &kp COMMA, &kp DOT, &u_to_U_BASE,\
+      U_NP, U_NP, &kp LSHIFT, &kp SPACE, &kp TAB,     &kp RET, U_LT(U_NUM, BSPC), &kp ESC, U_NP, U_NP
+// G, Z, X, C, V, B
+//&kp Q, &kp W, &kp E, &kp R, &kp T, &kp Y, &kp U, &kp I, &kp O, &kp P,        \
+//      U_MT(LGUI, A), U_MT(LALT, S), U_MT(LCTRL, D), U_MT(LSHFT, F), &kp G,     \
+//      &kp H, U_MT(LSHFT, J), U_MT(LCTRL, K), U_MT(LALT, L), U_MT(LGUI, SQT),   \
+//      U_LT(U_BUTTON, Z), U_MT(RALT, X), &kp C, &kp V, &kp B, &kp N, &kp M,     \
+//      &kp COMMA, U_MT(RALT, DOT), U_LT(U_BUTTON, SLASH), U_NP, U_NP,           \
+//      U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPACE), U_LT(U_MOUSE, TAB),              \
+//      U_LT(U_SYM, RET), U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL), U_NP, U_NP
 
 #define MIRYOKU_LAYER_NAV                                                      \
   U_BOOT, &u_to_U_TAP, &u_to_U_EXTRA, &u_to_U_BASE, &u_to_U_GAME, U_RDO,       \
       U_PST, U_CPY, U_CUT, U_UND, &kp LGUI, &kp LALT, &kp LCTRL, &kp LSHFT,    \
-      &u_to_U_POE, &u_caps_word, &kp LEFT, &kp DOWN, &kp UP, &kp RIGHT, U_NA,         \
-      &kp RALT, &u_to_U_NUM, &u_to_U_NAV, U_NA, &kp INS, &kp HOME, &kp PG_DN,  \
+      &u_to_U_LOL, &u_caps_word, &kp LEFT, &kp DOWN, &kp UP, &kp RIGHT, U_NA,  \
+      &kp RALT, &u_to_U_NUM, &u_to_U_NAV, &u_to_U_GW2, &kp INS, &kp HOME, &kp PG_DN,  \
       &kp PG_UP, &kp END, U_NP, U_NP, U_NA, U_NA, U_NA, &kp RET, &kp BSPC,     \
       &kp DEL, U_NP, U_NP
 
@@ -47,11 +61,12 @@
   MIRYOKU_X(SYM, "Sym")                                                        \
   MIRYOKU_X(FUN, "Fun")                                                        \
   MIRYOKU_X(GAME, "Game")                                                      \
-  MIRYOKU_X(POE, "POE")
+  MIRYOKU_X(LOL, "LoL")\
+  MIRYOKU_X(GW2, "GW2")
 
 #define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
-#define MIRYOKU_LAYERMAPPING_POE MIRYOKU_MAPPING
-
+#define MIRYOKU_LAYERMAPPING_LOL MIRYOKU_MAPPING
+#define MIRYOKU_LAYERMAPPING_GW2 MIRYOKU_MAPPING
 
 #define U_BASE 0
 #define U_EXTRA 1
@@ -64,4 +79,5 @@
 #define U_SYM 8
 #define U_FUN 9
 #define U_GAME 10
-#define U_POE 11
+#define U_LOL 11
+#define U_GW2 12
