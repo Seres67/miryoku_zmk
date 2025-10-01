2b118c26a47996c03fcbc2174ab089f173082a51
seres <seres@seres.eu.org>
2025-10-01 12:52:50 +0200

    uhh like this?

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -5,6 +5,37 @@
 
 #define XXX &none
 
+#define MIRYOKU_LAYER_LIST                                                     \
+  MIRYOKU_X(BASE, "Base")                                                      \
+  MIRYOKU_X(EXTRA, "Extra")                                                    \
+  MIRYOKU_X(TAP, "Tap")                                                        \
+  MIRYOKU_X(BUTTON, "Button")                                                  \
+  MIRYOKU_X(NAV, "Nav")                                                        \
+  MIRYOKU_X(MOUSE, "Mouse")                                                    \
+  MIRYOKU_X(MEDIA, "Media")                                                    \
+  MIRYOKU_X(NUM, "Num")                                                        \
+  MIRYOKU_X(SYM, "Sym")                                                        \
+  MIRYOKU_X(FUN, "Fun")                                                        \
+  MIRYOKU_X(GAME, "Game")                                                      \
+  MIRYOKU_X(LOL, "LoL")                                                        \
+  MIRYOKU_X(GW2, "GW2")                                                        \
+  MIRYOKU_X(GW2W, "GW2W")
+
+#define U_BASE 0
+#define U_EXTRA 1
+#define U_TAP 2
+#define U_BUTTON 3
+#define U_NAV 4
+#define U_MOUSE 5
+#define U_MEDIA 6
+#define U_NUM 7
+#define U_SYM 8
+#define U_FUN 9
+#define U_GAME 10
+#define U_LOL 11
+#define U_GW2 12
+#define U_GW2W 13
+
 #define MIRYOKU_LAYER_GAME                                                     \
   &kp LCTRL, &kp Q, &kp W, &kp E, &kp R, &kp T, &kp Y, &kp U, &kp I, &kp O,    \
       &kp LSHFT, &kp A, &kp S, &kp D, &kp F, &kp G, &kp H, &kp J, &kp K,       \
@@ -49,38 +80,7 @@
       K16 K17 K18 K19 XXX XXX K20 K21 K22 K23 K24 K25 K26 K27 K28 K29 XXX K32  \
           K33 K34 K35 K36 K37
 
-#define MIRYOKU_LAYER_LIST                                                     \
-  MIRYOKU_X(BASE, "Base")                                                      \
-  MIRYOKU_X(EXTRA, "Extra")                                                    \
-  MIRYOKU_X(TAP, "Tap")                                                        \
-  MIRYOKU_X(BUTTON, "Button")                                                  \
-  MIRYOKU_X(NAV, "Nav")                                                        \
-  MIRYOKU_X(MOUSE, "Mouse")                                                    \
-  MIRYOKU_X(MEDIA, "Media")                                                    \
-  MIRYOKU_X(NUM, "Num")                                                        \
-  MIRYOKU_X(SYM, "Sym")                                                        \
-  MIRYOKU_X(FUN, "Fun")                                                        \
-  MIRYOKU_X(GAME, "Game")                                                      \
-  MIRYOKU_X(LOL, "LoL")                                                        \
-  MIRYOKU_X(GW2, "GW2")                                                        \
-  MIRYOKU_X(GW2W, "GW2W")
-
 #define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
 #define MIRYOKU_LAYERMAPPING_LOL MIRYOKU_MAPPING
 #define MIRYOKU_LAYERMAPPING_GW2 MIRYOKU_MAPPING
 #define MIRYOKU_LAYERMAPPING_GW2W MIRYOKU_MAPPING
-
-#define U_BASE 0
-#define U_EXTRA 1
-#define U_TAP 2
-#define U_BUTTON 3
-#define U_NAV 4
-#define U_MOUSE 5
-#define U_MEDIA 6
-#define U_NUM 7
-#define U_SYM 8
-#define U_FUN 9
-#define U_GAME 10
-#define U_LOL 11
-#define U_GW2 12
-#define U_GW2W 13
